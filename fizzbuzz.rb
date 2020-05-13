a810730c777ba8f0e13aa19672d8eeee828b83fc
Ivan Ivanov Bukev <ibukev4@gmail.com>
2020-05-13 10:58:33 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(i)
-  if i % 3 == 0 && i % 5 == 0
-    "FizzBuzz"
-  else if i % 3 == 0
-    "Fizz"
-  else if i % 5 == 0
-    "Buzz"
-  else
-
-  end
+  if
+    i % 3 == 0 && i % 5 == 0
+       "FizzBuzz"
+elseif i % 3 == 0
+      "Fizz"
+elseif i % 5 == 0
+      "Buzz"
+    else
+end
 end
